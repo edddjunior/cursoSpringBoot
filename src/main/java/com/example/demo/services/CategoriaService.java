fa8e51e953e69039e96ef664f3d8d7386603c5e7
edddjunior <edsonbergamojunior@gmail.com>
2021-01-25 23:48:07 -0300

    Categorias POST

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -22,4 +22,10 @@ public class CategoriaService {
 			"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
 		)); 
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
