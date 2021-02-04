565eedeefdb4c7ea7fb2d0c98c47cc2d11551b51
edddjunior <edsonbergamojunior@gmail.com>
2021-02-03 23:44:56 -0300

    ajustes na identação

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -39,10 +39,11 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/page", method = RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page, 
-			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
-			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		@RequestParam(value = "page", defaultValue = "0") Integer page, 
+		@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+		@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction
+	) {
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		
