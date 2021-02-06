371ca31e34e6830b9b3eaaf308c0a9dd735dc61a
edddjunior <edsonbergamojunior@gmail.com>
2021-02-06 19:00:39 -0300

    Cálculo de subtotal e total

--- src/main/java/com/example/demo/domain/ItemPedido.java
+++ src/main/java/com/example/demo/domain/ItemPedido.java
@@ -33,6 +33,10 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	public double getSubtotal() {
+		return (preco - desconto) * quantidade;
+	}
 
 	@JsonIgnore
 	public Pedido getPedido() {
