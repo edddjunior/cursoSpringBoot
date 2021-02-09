fb142a76158de557bcb37ca4d1eca747ff20975a
edddjunior <edsonbergamojunior@gmail.com>
2021-02-09 03:34:26 -0300

    Envio de email funcionando

--- src/main/java/com/example/demo/services/DBService.java
+++ src/main/java/com/example/demo/services/DBService.java
@@ -122,7 +122,7 @@ public class DBService {
 		
 		
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "extra.edddjunior@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
