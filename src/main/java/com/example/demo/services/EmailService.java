0dcfaba1b77c70b636a872c3211302319c955727
edddjunior <edsonbergamojunior@gmail.com>
2021-02-09 04:21:41 -0300

    Envio de email html funcionando

--- src/main/java/com/example/demo/services/EmailService.java
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.example.demo.domain.Pedido;
@@ -9,5 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
--- src/main/java/com/example/demo/services/SmtpEmailService.java
+++ src/main/java/com/example/demo/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.example.demo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
@@ -13,12 +16,22 @@ public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando email...");
 		mailSender.send(msg);
 		LOG.info("Email enviado");		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");			
 	}
 
 }
